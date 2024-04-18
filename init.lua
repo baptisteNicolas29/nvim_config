5c89bebe872d2df2a67387d28f68ebc9585ff194
Baptiste <baptiste.nicolas@protonmail.com>
2024-04-19 00:27:37 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,27 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+-- install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugin installation system
+-- you can setup your plugin structure inside ./lua/plugins.lua
+require("lazy").setup("plugins")
+
+-- require("nvim-tree").setup()
+require("lualine").setup()
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        PATH = "prepend", -- "skip" seems to cause the spawning error
+      })
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "pylsp"}
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup {}
+      lspconfig.pylsp.setup {
+        jedi_completion = {fuzzy = true}
+      }
+      vim.keymap.set('n', 'gh', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, 'ga', vim.lsp.buf.code_action, {})
+    end
+  },
+}
