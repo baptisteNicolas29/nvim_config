327062a80d46f3ccb224c9b4adbfd921fc8a5f8a
Baptiste <baptiste.nicolas@protonmail.com>
2024-12-20 10:59:23 +0100

    add areal to my config

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+     "nvim-treesitter/nvim-treesitter",
+     "nvim-tree/nvim-web-devicons"
+  },
+
+  config = function()
+    -- example mapping to toggle outline
+    -- vim.keymap.set("n", "<leader>o", "<cmd>outline<cr>", { desc = "toggle outline" })
+
+    require("aerial").setup {
+      -- your setup opts here (leave empty to use defaults)
+      manage_folds = true,
+    }
+  end,
+  -- You probably also want to set a keymap to toggle aerial
+  vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+}
